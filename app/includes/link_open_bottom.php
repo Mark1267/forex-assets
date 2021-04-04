e8c0fb093dbaf731511e41eddfa0139833e92307
Mark1267 <markwillams1267@gmail.com>
2021-04-04 23:45:40 +0100

    added tidio

--- app/includes/link_open_bottom.php
+++ app/includes/link_open_bottom.php
@@ -31,16 +31,4 @@
           itemsDesktopSmall: [979, 2]
       });
 </script>
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6066306ef7ce182709364b79/1f27iqgkt';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
+<script src="//code.tidio.co/mtisrohcut78uaha8qkxrqcqmhwskm0v.js" async></script>
