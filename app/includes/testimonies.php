8482d5c3371e8c5d533b4ff3de568bd8c124f2a1
Mark1267 <markwillams1267@gmail.com>
2021-04-05 08:48:56 +0100

    u05042021

--- app/includes/testimonies.php
+++ app/includes/testimonies.php
@@ -7,6 +7,7 @@
         <p class="heading_space">We are trusted by thousands of users across the globe.</p>
       </div>
     </div>
+    <style>.iklk{border-radius: 50%;}</style>
     <div id="people_slider" class="owl-carousel">
       <div class="item">
         <div class="people_wrap border_radius left">
@@ -14,7 +15,7 @@
         <p>Standing idle is not an option when your core business is security. ZENEX is proud to partner with FOREX-ASSETS because our goals converge around a common pillar — trust. The projects in prospect are exciting and we are looking forward to providing an expanded portfolio of solutions, including distributed ledger and cryptographic technologies to respond to our clients’ shifting needs.</p>
         </div>
         <div class="testinomial_pic">
-          <div class="pic"><imgwidth="58px" alt="testinomial" src="<?php echo BASE_URL . '/assets/dashboard/images/user/avatar-s-19.png'; ?>"></div>
+          <div class="pic"><img class="iklk" width="58px" alt="testinomial" src="<?php echo BASE_URL . '/assets/open/images/2.jpg'; ?>"></div>
           <div class="testinomial_body">
             <span class="name">John Smith</span>
             <span class="post_img">Client</span>
@@ -31,7 +32,7 @@
             <span class="name">Ellizabeth Taylor</span>
             <span class="post_img">Client</span>
           </div>
-          <div class="pic"><img width="58px" alt="testinomial" src="<?php echo BASE_URL . '/assets/dashboard/images/user/avatar-s-19.png'; ?>"></div>
+          <div class="pic"><img width="58px" class="iklk" alt="testinomial" src="<?php echo BASE_URL . '/assets/open/images/1.jpg'; ?>"></div>
         </div>
       </div>
       <div class="item">
@@ -40,7 +41,7 @@
         <p>With FOREX-ASSETS, our aim is to help the financial sector with the opportunities and challenges of the blockchain technology by offering outstanding solutions in the field of distributed ledgers, cryptocurrencies and digital assets.</p>
         </div>
         <div class="testinomial_pic">
-          <div class="pic"><img width="58px" alt="testinomial" src="<?php echo BASE_URL . '/assets/dashboard/images/user/avatar-s-19.png'; ?>"></div>
+          <div class="pic"><img width="58px" class="iklk" alt="testinomial" src="<?php echo BASE_URL . '/assets/open/images/3.jpg'; ?>"></div>
           <div class="testinomial_body">
             <span class="name">Bard Markson</span>
             <span class="post_img">Client</span>
@@ -54,10 +55,10 @@
         </div>
         <div class="testinomial_pic text-right pull-right">
           <div class="testinomial_body">
-            <span class="name">Joe Davison</span>
+            <span class="name">Sarah E. Davison</span>
             <span class="post_img">Client</span>
           </div>
-          <div class="pic"><img width="58px" alt="testinomial" src="<?php echo BASE_URL . '/assets/dashboard/images/user/avatar-s-19.png'; ?>"></div>
+          <div class="pic"><img width="58px" class="iklk" alt="testinomial" src="<?php echo BASE_URL . '/assets/open/images/6.jpg'; ?>"></div>
         </div>
       </div>
     </div>
