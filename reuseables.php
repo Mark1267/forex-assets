2b7a29df8adeebcdcfb2c6084fc784c396d6f921
Mark Willams <81035184+Mark1267@users.noreply.github.com>
2021-07-07 18:52:30 +0100

    Update reuseables.php
    Changed mail

--- reuseables.php
+++ reuseables.php
@@ -4,8 +4,8 @@ $reuseables = array(
     "XVIDEO" => array('mp4', 'mkv', 'avi', '3gp'),
     "XAUDIO" => array('mp3', 'm4a', 'wav', 'opus'),
     "XFILE" => array('txt', 'pdf', 'docx'),
-    "UMAIL" => 'support@forex-assets.exchange',
-    "PMAIL" => 'David032021'
+    "UMAIL" => 'support@wievatrade.com',
+    "PMAIL" => 'Wievatrade06'
 );
 
 foreach($reuseables as $key => $value){
